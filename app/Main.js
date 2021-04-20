aab636b708b5617289c54ce36d959d744f3ac532
Porfidio alicea <porfidio@ptd.net>
2021-04-20 17:22:51 -0400

    Initial Coding

---
+++ app/Main.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import  {BrowserRouter, Switch, Route}from "react-router-dom"
+// My Components
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
+
+
+function Main() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact><HomeGuest /></Route>
+        <Route path="/About"><About /></Route>
+        <Route path="/Terms" ><Terms /></Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+ReactDOM.render(<Main />, document.querySelector("#app"));
+
+if (module.hot) {
+  module.hot.accept();
+}
